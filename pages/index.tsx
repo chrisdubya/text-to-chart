036a7bc2bf9b9681ce274824587c95588a22fc05
Chris Williams <chris.r.williams@icloud.com>
2022-09-06 20:21:49 -0400

    scaffold d3 with generic barchart and dummy data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,8 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import Chart from "../components/Chart";
 
 const Home: NextPage = () => {
-	return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+	return <Chart />;
 };
 
 export default Home;
